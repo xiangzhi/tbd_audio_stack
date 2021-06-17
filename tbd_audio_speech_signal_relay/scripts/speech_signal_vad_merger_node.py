3c51bb695f7b07bab2b53d8bc90e43c5ed9b747e
Zhi Tan <zhi.tan@ri.cmu.edu>
2021-06-16 22:50:03 -0400

    handle multiple signal inputs.

--- tbd_audio_speech_signal_relay/scripts/speech_signal_vad_merger_node.py
+++ tbd_audio_speech_signal_relay/scripts/speech_signal_vad_merger_node.py
@@ -1,33 +1,39 @@
 #!/usr/bin/env python3
 
+import sys
 import rospy
 import message_filters
 from std_msgs.msg import Bool
 from tbd_audio_msgs.msg import VADStamped
+
+THRESHOLD = 0.2  # Number of seconds before true gets published again
 
 
 def main():
 
     rospy.init_node("agent_vad_speech_signal_merger", anonymous=True)
 
-    THRESHOLD = 0.15
-    last_speech_signal = None
+    threshold = rospy.Duration(secs=THRESHOLD)
+    last_signal = None
     con_pub = rospy.Publisher('vad_out', VADStamped, queue_size=1)
 
     def signal_cb(msg: Bool):
         if (msg.data):
-            nonlocal last_speech_signal
-            last_speech_signal = rospy.Time.now()
+            nonlocal last_signal
+            last_signal = rospy.Time.now()
 
     def vad_cb(msg: VADStamped):
-        if last_speech_signal is not None and abs((msg.header.stamp - last_speech_signal).to_sec()) < THRESHOLD: 
+        if last_signal is not None and (msg.header.stamp < last_signal or
+                                        (msg.header.stamp - last_signal) < threshold):
             msg.is_speech = False
         con_pub.publish(msg)
 
-    rospy.Subscriber('vad',VADStamped,vad_cb, queue_size=1)
-    rospy.Subscriber('speak_signal', Bool, signal_cb, queue_size=1)
+    for topic_name in sys.argv:
+        rospy.Subscriber(topic_name, Bool, signal_cb, queue_size=1)
 
+    rospy.Subscriber('vad', VADStamped, vad_cb, queue_size=1)
     rospy.spin()
+
 
 if __name__ == "__main__":
     main()
